7e9677ee7b0795d1a2d2fe97835d9693c08a3559
fab1o978 <fabio.978@gmail.com>
2018-10-19 11:37:21 +0200

    Added commands

--- GoPro_WiFi.cpp
+++ GoPro_WiFi.cpp
@@ -4,38 +4,52 @@
 #include <ESP8266WiFiMulti.h>
 #include <ESP8266HTTPClient.h>
 
+// const string GoPro_WiFi::GP_STATUS = "http://10.5.5.9/bacpac/sx?t={password}";
+
 GoPro_WiFi::GoPro_WiFi(char* SSID, char* password)
 {
     _SSID = SSID;
     _password = password;
+
+    _commands[GP_STATUS] =  "http://10.5.5.9/camera/sx?t={password}";
+    _commands[GP_POWER_ON] =  "http://10.5.5.9/bacpac/PW?t={password}&p=%01";
+    _commands[GP_POWER_OFF] = "http://10.5.5.9/bacpac/PW?t={password}&p=%00";
+    _commands[GP_TAKE_PICTURE] =  "http://10.5.5.9/bacpac/SH?t={password}&p=%01";
 }
 
 void GoPro_WiFi::connect()
 {
-    // ESP8266WiFiMulti WiFiMulti;
-
     WiFi.mode(WIFI_STA);
     WiFiMulti.addAP(_SSID, _password);
 
     Serial.println("Added SSID: " + String(_SSID));
 }
 
-int GoPro_WiFi::command(String command, boolean secure = false)
+int GoPro_WiFi::command(String command)
 {
     HTTPClient http;
 
-    if(secure)
-        command = command + "t=" + String(_password);
+    if(command.indexOf("{password}") != -1){
+        command.replace("{password}", _password);
+    }
 
-    if (http.begin("http://10.5.5.9/" + command)) {
+    Serial.print("Command: ");
+    Serial.println(command);
+
+    if (http.begin(command)) {
         int httpCode = http.GET();
-        //USE_SERIAL.print("HTTP Code: ");
-        //USE_SERIAL.println(httpCode);
 
         http.end();
 
         return httpCode;
+    } else {
+        Serial.println("http error!");
     }
+}
+
+int GoPro_WiFi::command(gopro_commands_t commandID)
+{
+    return command(_commands[commandID]);
 }
 
 wl_status_t GoPro_WiFi::status()
--- GoPro_WiFi.h
+++ GoPro_WiFi.h
@@ -6,18 +6,33 @@
 #include <ESP8266WiFiMulti.h>
 #include <ESP8266HTTPClient.h>
 
+typedef enum {
+    ACTION_POWER_ON = 1
+} gopro_actions_t;
+
+typedef enum {
+  GP_STATUS = 0,
+  GP_TAKE_PICTURE = 1,
+  GP_POWER_ON = 2,
+  GP_POWER_OFF = 3
+  //GOPRO_STATUS = "http://10.5.5.9/bacpac/sx?t={password}",
+  //GOPRO_TAKE_PICTURE = "http://10.5.5.9/bacpac/SH?t={password}&p=%01"
+} gopro_commands_t;
+
 class GoPro_WiFi
 {
   public:
     GoPro_WiFi(char* SSID, char* password);
     void connect();
     wl_status_t status();
-    int command(String command, boolean secure);
+    int command(String command);
+    int command(gopro_commands_t command);
     ESP8266WiFiClass WiFi;
     ESP8266WiFiMulti WiFiMulti;
   private:
     char* _SSID;
     char* _password;
+    String _commands[10];
 };
 
 #endif
